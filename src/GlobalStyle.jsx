8b98fedf8f2eda6019fcbbe9c01e7510f7d4a4d3
Yujin Kim <cocodj0509@naver.com>
2023-06-26 19:15:57 +0900

    feat: 네비게이션바

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    button {
+        border: 0;
+        cursor: pointer;
+        transition: all 0.4s;
+    }
+`;
+
+export default GlobalStyle;
