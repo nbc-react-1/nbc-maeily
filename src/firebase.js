5e99a6a7640a384f695b2723cfa899e3427956ab
heejung-newheee <aldls2311@gmail.com>
2023-06-27 21:19:53 +0900

    Feat: firebase 등록 기능

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,19 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-const APP_ID = process.env.REACT_APP_APP_ID;
-
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export default app;
 export const auth = getAuth(app);
-export const storage = getStorage();
 export const db = getFirestore(app);
+export const storage = getStorage(app);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,82 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
-import List from '../components/List';
 import { StButton, ButtonWrap } from '../components/Button';
-import { app } from '../firebase';
-import { getDocs, collection } from 'firebase/firestore';
-import { db } from '../firebase';
+import { getDocs, collection, addDoc } from 'firebase/firestore';
+import { app, db, storage } from '../firebase';
+import { ref, uploadBytes } from 'firebase/storage';
+import { initializeApp } from 'firebase/app';
 
 const Home = () => {
+  const [post, setPost] = useState([
+    {
+      id: 0,
+      image: 'url?',
+      contents: '내용입니다',
+    },
+    {
+      id: 1,
+      image: 'url?',
+      contents: '내용입니다',
+    },
+    {
+      id: 2,
+      image: 'url?',
+      contents: '내용입니다',
+    },
+    {
+      id: 3,
+      image: 'url?',
+      contents: '내용입니다',
+    },
+  ]);
+  // 데이터 리스트로 불러오기
+  useEffect(() => {
+    const initialPostItem = [];
+    const fetchData = async () => {
+      // const queryValue = query();
+      const querySnapshot = await getDocs(collection(db, 'post-item'));
+
+      querySnapshot.forEach(doc => {
+        const data = {
+          id: doc.id,
+          ...doc.data(),
+        };
+        console.log('data', data);
+        initialPostItem.push(data);
+      });
+    };
+    // fetchData();
+    setPost(initialPostItem);
+  }, []);
+
+  //게시글 등록
+  const [contents, setContents] = useState('');
+  // const [image, setImage] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const selectFile = event => {
+    setSelectedFile(event.target.files[0]);
+  };
+  // 사진올리기
+  const addPost = async e => {
+    e.preventDefault();
+    const imageRef = ref(storage, 'folder/file');
+    uploadBytes(imageRef, selectedFile);
+  };
+  // 글올리기
+  const postHandler = async e => {
+    e.preventDefault();
+    const newAddPost = {
+      selectedFile,
+      contents,
+    };
+    // const newPostList = [...post, newAddPost];
+    // setPost(newPostList); //화면에 보여주는 추가
+    const collectionRef = collection(db, 'post-item');
+    await addDoc(collectionRef, newAddPost);
+    setSelectedFile('');
+    setContents('');
+  };
+
   return (
     <>
       {/* 상단 게시글 등록 버튼  */}
@@ -14,10 +84,46 @@ const Home = () => {
         <StBanner>
           <StH1>maeilyLook</StH1>
         </StBanner>
-        <List />
+        <StCardContainer>
+          {post.map(item => {
+            return (
+              <StCard>
+                <StImg src="" alt={item.image} />
+                <StId>{item.id}</StId>
+                <StContent>{item.contents}</StContent>
+              </StCard>
+            );
+          })}
+        </StCardContainer>
       </div>
 
-      <CreatePost />
+      {/* modal */}
+      <div>
+        <ModalBg>
+          <ModalContents>
+            {/* 수정페이지에서 보여줘야함 */}
+            <ButtonWrap style={{ float: 'right' }}>
+              <StButton acColor={'#39ddc2'}>수정</StButton>
+              <StButton acColor={'#39ddc2'}>삭제</StButton>
+            </ButtonWrap>
+
+            <form onSubmit={postHandler} style={{ clear: 'both', overflow: 'hidden' }}>
+              <Label>사진 첨부 </Label>
+              <Input type="file" onChange={selectFile} />
+              <Label>내용</Label>
+              <InputArea
+                value={contents}
+                onChange={event => {
+                  setContents(event.target.value);
+                }}
+              />
+              <StButton type="submit" onClick={addPost} style={{ float: 'right' }} acColor={'#39ddc2'}>
+                등록
+              </StButton>
+            </form>
+          </ModalContents>
+        </ModalBg>
+      </div>
     </>
   );
 };
@@ -36,46 +142,40 @@ const StH1 = styled.h1`
   color: white;
   font-size: 50px;
 `;
+// list style
+const StCardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
+  max-width: 1300px;
+  min-width: 800px;
+  margin: 0 auto;
+  padding-top: 50px;
+`;
+const StCard = styled.div`
+  border: none;
+  width: 300px;
+  height: 500px;
+  cursor: pointer;
+`;
+const StImg = styled.img`
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
+  border-radius: 10px;
+`;
+const StId = styled.p`
+  margin-top: 10px;
+  font-weight: bold;
+  font-size: 20px;
+`;
+const StContent = styled.p`
+  margin-top: 10px;
+  font-weight: bold;
+  font-size: 15px;
+`;
 
-const CreatePost = () => {
-  useEffect(() => {
-    console.log('app', app);
-  }, []);
-  useEffect(() => {
-    const fetchData = async () => {
-      const querySnapshot = await getDocs(collection(db, 'post-item'));
-      const initialPostItem = [];
-      querySnapshot.forEach(doc => {
-        console.log(`${doc.id} => ${doc.data()}`);
-      });
-    };
-    fetchData();
-  }, []);
-  return (
-    <>
-      <ModalBg>
-        <ModalContents>
-          {/* 수정페이지에서 보여줘야함 */}
-          <ButtonWrap style={{ float: 'right' }}>
-            <StButton acColor={'#39ddc2'}>수정</StButton>
-            <StButton acColor={'#39ddc2'}>삭제</StButton>
-          </ButtonWrap>
-
-          <form action="" style={{ clear: 'both', overflow: 'hidden' }}>
-            <Label>사진 첨부 </Label>
-            <Input />
-            <Label>내용</Label>
-            <InputArea />
-          </form>
-          <StButton style={{ float: 'right' }} acColor={'#39ddc2'}>
-            등록
-          </StButton>
-        </ModalContents>
-      </ModalBg>
-    </>
-  );
-};
-
+// modal
 const ModalBg = styled.div`
   width: 100%;
   height: 100%;
@@ -85,7 +185,7 @@ const ModalBg = styled.div`
   left: 0;
 `;
 const ModalContents = styled.div`
-  width: 60%;
+  width: 30%;
   background-color: #fff;
   border-radius: 10px;
   box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
