93d8e0d93f6c4ca73279b5f6eb30e79777a3623c
heejung-newheee <aldls2311@gmail.com>
2023-06-27 11:17:33 +0900

    Test: 환경변수 설정 업로드 테스트

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const APP_ID = process.env.REACT_APP_APP_ID;
 
 // Your web app's Firebase configuration
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: toString(API_KEY),
+  authDomain: 'maeily.firebaseapp.com',
+  projectId: 'maeily',
+  storageBucket: 'maeily.appspot.com',
+  messagingSenderId: '621274436632',
+  appId: toString(APP_ID),
+};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export default app;
 export const auth = getAuth(app);
 export const storage = getStorage();
