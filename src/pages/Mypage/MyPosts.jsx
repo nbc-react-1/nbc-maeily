8cf53ab15d1d2bbaf75ccafb2972a12b238b58ed
heejung-newheee <aldls2311@gmail.com>
2023-06-30 19:08:35 +0900

    Feat: 게시글 수정/삭제

--- src/pages/Mypage/MyPosts.jsx
+++ src/pages/Mypage/MyPosts.jsx
@@ -1,33 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import { keyframes, styled } from 'styled-components';
-import { collection, getDocs, query } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs, query } from 'firebase/firestore';
 import { db } from '../../firebase';
 import { useSelector } from 'react-redux';
 import EditPostModal from '../../components/modal/EditPostModal';
 
 function MyPosts() {
   const { storeInfo, isUserTrue } = useSelector(state => state.userLogIn);
-  const [editList, setEditList] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [post, setPost] = useState([]);
   const [contents, setContents] = useState('');
   const [selectedFile, setSelectedFile] = useState(null);
+
+  const [editList, setEditList] = useState(false);
+  const [editItemId, setEditItemId] = useState('');
   const [itemId, setItemId] = useState('');
+  const [updatePhoto, setUpdatePhoto] = useState(false);
 
   const openModal = () => {
     setItemId('');
     setEditList(false);
-    setIsOpen(true);
+    setIsOpen(!isOpen);
   };
   const closeModal = () => {
-    setIsOpen(false);
+    setIsOpen(!isOpen);
     setContents('');
     setSelectedFile(null);
+    console.log('closeModal');
   };
-
+  // const updateHandler = () => {
+  //   setUpdatePhoto(!updatePhoto);
+  // };
+  console.log('updatePhoto 전체 reload 중...?', updatePhoto);
+  console.log('isOpen 전체 reload 중...?', isOpen);
   const { profileImg, nickname } = storeInfo;
 
   useEffect(() => {
+    console.log(updatePhoto, 'updatePhoto111111');
+    console.log(isOpen, 'isOpen1111111');
     const initialPostItem = [];
     const fetchData = async () => {
       const queryValue = query(collection(db, 'post-item'));
@@ -42,9 +52,29 @@ function MyPosts() {
       setPost(initialPostItem);
     };
     fetchData();
-  }, []);
+  }, [updatePhoto]);
 
+  // 게시글 삭제
+  const deletePost = async () => {
+    console.log('삭제');
+    setEditList(false); //안됨 안바뀜
+    console.log(editList);
+
+    const deletePostConfirm = window.confirm('정말로 삭제하시겠습니까?');
+    if (deletePostConfirm) {
+      await deleteDoc(doc(db, 'post-item', editItemId))
+        .then(() => {
+          alert('삭제가 정상적으로 처리되었습니다.');
+        })
+        .catch(error => {
+          console.log('errorCode', error.code);
+        });
+    } else alert('취소 하셨습니다.');
+  };
+
+  // edit 버블 열기
   const openBubble = id => {
+    setEditItemId(id);
     if (editList) {
       setItemId('');
       setEditList(!editList);
@@ -70,7 +100,12 @@ function MyPosts() {
                 <StContent>{item.contents}</StContent>
               </StContents>
               <StEditButton>
-                <StEditButtonIcon onClick={() => openBubble(item.id)}>
+                <StEditButtonIcon
+                  onClick={() => openBubble(item.id)}
+                  // onBlur={() => {
+                  //   setEditList(false);
+                  // }}
+                >
                   <SettingSvg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512">
                     <path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" />
                   </SettingSvg>
@@ -79,7 +114,7 @@ function MyPosts() {
                   <StEditBubble>
                     <ul>
                       <li onClick={openModal}>수정</li>
-                      <li>삭제</li>
+                      <li onClick={deletePost}>삭제</li>
                     </ul>
                   </StEditBubble>
                 ) : null}
@@ -87,7 +122,20 @@ function MyPosts() {
             </StCard>
           );
         })}
-      <EditPostModal isUserTrue={isUserTrue} isOpen={isOpen} closeModal={closeModal} selectedFile={selectedFile} setSelectedFile={setSelectedFile} contents={contents} setContents={setContents} post={post} setPost={setPost} />
+      <EditPostModal
+        editItemId={editItemId}
+        isUserTrue={isUserTrue}
+        isOpen={isOpen}
+        closeModal={closeModal}
+        selectedFile={selectedFile}
+        setSelectedFile={setSelectedFile}
+        contents={contents}
+        setContents={setContents}
+        post={post}
+        setPost={setPost}
+        updatePhoto={updatePhoto}
+        setUpdatePhoto={setUpdatePhoto}
+      />
     </StCardContainer>
   );
 }
